b832af01d505ab4df1299ac2a81593a30e5e0802
Jwon <check4545@kakao.com>
2023-07-04 23:22:38 +0900

    Implement 영어 단어 Quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styled from "styled-components";
+
+const synth = window.speechSynthesis;
+
+const Word = [
+	{
+		word: "World",
+		meaning: "세계, 지구"
+	},
+	{
+		word: "Head",
+		meaning: "머리"
+	}
+];
+
+// TODO
+// 각각 직접 채점 방식 -> 제출 시 전체 한번에 자동 채점 방식
+const temp = {};
+
+const WordSection = ({index, value}) => {
+	
+	const utterance = new SpeechSynthesisUtterance();
+	utterance.lang = "en-US"
+	utterance.text = value.word
+	
+	const [word, setWord] = useState("");
+	const [meaning, setMeaning] = useState("");
+	
+	const [isDisabled, setIsDisabled] = useState(false);
+	
+	const [isCorrect, setIsCorrect] = useState(null);
+	
+	const answerCheck = (word, meaning) => {
+		word = word.toUpperCase();
+		const meaningArr = value.meaning.split(", ");
+		
+		setIsDisabled(true);
+		
+		if(word == value.word.toUpperCase() && meaningArr.includes(meaning)){
+			window.confirm("정답");
+			setIsCorrect(true);
+			return
+		}
+		
+		window.confirm("오답");
+		setIsCorrect(false);
+		return
+
+	}
+	
+	return (
+		<QuizSection
+			key={index}
+		>
+			<div>
+				{index}번
+				<Button onClick={() => synth.speak(utterance)}>
+					단어듣기
+				</Button>
+			</div>
+			
+			<AnswerSection>
+				<Input
+					isCorrect = {isCorrect}
+					placeholder={"단어를 입력하세요."}
+					onChange={(e) => setWord(e.target.value)}
+					// onChange={(e) => {
+					// 	temp[index] = {...temp[index], word: e.target.value};
+					// 	console.log(temp)
+					// }}
+					disabled={isDisabled}
+				>
+				
+				</Input>
+				
+				<Input
+					isCorrect = {isCorrect}
+					placeholder={"뜻을 입력하세요."}
+					onChange={(e) => setMeaning(e.target.value)}
+					// onChange={(e) => {
+					// 	temp[index] = {...temp[index], meaning: e.target.value};
+					// 	console.log(temp)
+					// }}
+					disabled={isDisabled}
+				>
+				
+				</Input>
+				
+				<Button onClick={() => answerCheck(word, meaning)}>
+					정답 확인
+				</Button>
+			</AnswerSection>
+		</QuizSection>
+	)
 }
+
+
+
+
+const App = () => {
+	
+	
+	return (
+		<Container>
+			{
+				Word.map((v, i) => (
+					<WordSection index={i} value={v}/>
+				))
+			}
+		
+		</Container>
+	);
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const QuizSection = styled.div`
+  height: 100px;
+  border: 1px solid;
+  padding: 15px;
+`;
+
+const AnswerSection = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Input = styled.input`
+	background: ${props => props.isCorrect == null ? "" : (props.isCorrect  ? "#9cdc9e" : "#dc7f81")};
+  	color: black;
+  	font-size: 25px;
+`;
+
+const Button = styled.button`
+
+`;
 
 export default App;
