e9520a2de8e925ac941fcbd7b3d21ce618be3242
Jwon <check4545@kakao.com>
2023-07-16 00:55:52 +0900

    Change Words

--- src/App.js
+++ src/App.js
@@ -8,192 +8,184 @@ const synth = window.speechSynthesis;
 // 단어 섞는 부분 분리
 const Word = [
     {
-        "word": "develop",
-        "meaning": "발전하다, 발달하다"
+        "word": "discourage",
+        "meaning": "그만두게 하다, 단념시키다, 낙담시키다, 실망시키다"
     },
     {
-        "word": "derive",
-        "meaning": "이끌어내다, 얻다"
+        "word": "discount",
+        "meaning": "할인, 할인하다, 중요하지 않게 생각하다"
     },
     {
-        "word": "undergraduate",
-        "meaning": "대학생(의), 학부생(의), 대학생의, 학부생의"
+        "word": "inevitable",
+        "meaning": "피할 수 없는, 불가피한"
     },
     {
-        "word": "represent",
-        "meaning": "대변하다, 대표하다, 나타내다"
+        "word": "uneasy",
+        "meaning": "불안한, 불편한, 걱정스러운, 편치 않은"
     },
     {
-        "word": "evaporation",
-        "meaning": "증발, 발산"
+        "word": "disappear",
+        "meaning": "사라지다, 없어지다, 소멸하다"
     },
     {
-        "word": "dialect",
-        "meaning": "방언, 사투리"
+        "word": "unfortunate",
+        "meaning": "불운한, 불행한, 유감스러운"
     },
     {
-        "word": "research",
-        "meaning": "연구, 조사, 연구하다"
+        "word": "display",
+        "meaning": "전시하다, 진열하다, 보이다, 표현하다, 전시, 진열, 보이기, 표현"
     },
     {
-        "word": "reproduce",
-        "meaning": "복제하다, 번식하다"
+        "word": "support",
+        "meaning": "지지하다, 부양하다, 지지, 받치다, 지원, 받침"
     },
     {
-        "word": "retire",
-        "meaning": "은퇴하다, 퇴직하다"
+        "word": "dismiss",
+        "meaning": "묵살하다, 일축하다, 해고하다, 내쫓다, 해산시키다"
     },
     {
-        "word": "exhale",
-        "meaning": "내쉬다"
+        "word": "advantage",
+        "meaning": "이점, 유리한 조건, 유리한 입장"
     },
     {
-        "word": "detach",
-        "meaning": "분리하다, 떼어내다"
+        "word": "unusual",
+        "meaning": "보통이 아닌, 유별난, 색다른, 평소와 다른"
     },
     {
-        "word": "exaggerate",
-        "meaning": "과장하다"
+        "word": "superficial",
+        "meaning": "표면의, 외관상의, 피상적인"
     },
     {
-        "word": "expand",
-        "meaning": "팽창하다, 확장하다"
+        "word": "disagree",
+        "meaning": "동의하지 않다, 일치하지 않다, 다르다"
     },
     {
-        "word": "international",
-        "meaning": "국제적인, 국가 간의"
+        "word": "submarine",
+        "meaning": "잠수함, 해저의"
     },
     {
-        "word": "explicit",
-        "meaning": "뚜렷한, 명백한"
+        "word": "suppress",
+        "meaning": "진압하다, 억제하다, 참다, 억누르다"
     },
     {
-        "word": "exchange",
-        "meaning": "교환, 교환하다"
+        "word": "irrelevant",
+        "meaning": "관계가 없는, 상관없는"
     },
     {
-        "word": "declare",
-        "meaning": "선언하다, 발표하다"
+        "word": "upright",
+        "meaning": "똑바른, 수직으로 세운, 올바른, 정직한, 똑바로, 수직으로, 수직 기둥"
     },
     {
-        "word": "recycle",
-        "meaning": "재활용하다"
+        "word": "surface",
+        "meaning": "표면, 외관, 올라오다, 드러나다, 나타나다"
     },
     {
-        "word": "depict",
-        "meaning": "그리다, 묘사하다"
+        "word": "differ",
+        "meaning": "다르다, 일치하지 않다"
     },
     {
-        "word": "recall",
-        "meaning": "기억해내다, 회상하다"
+        "word": "ancient",
+        "meaning": "고대의, 옛날의"
     },
     {
-        "word": "remove",
-        "meaning": "제거하다, 옮기다"
+        "word": "antibiotic",
+        "meaning": "항생제, 항생 물질"
     },
     {
-        "word": "despise",
-        "meaning": "경멸하다, 얕보다"
+        "word": "disorder",
+        "meaning": "무질서, 혼란, 이상, 불편"
     },
     {
-        "word": "dialogue",
-        "meaning": "대화 문답, 회화"
+        "word": "sovereign",
+        "meaning": "통치자, 주권이 있는, 최고 권력의"
     },
     {
-        "word": "depress",
-        "meaning": "낙담시키다, 우울하게 하다"
+        "word": "upside",
+        "meaning": "위쪽, 윗면, "
     },
     {
-        "word": "depart",
-        "meaning": "떠나다, 출발하다"
+        "word": "immoral",
+        "meaning": "부도덕한"
     },
     {
-        "word": "revive",
-        "meaning": "소생시키다, 소생하다"
+        "word": "superb",
+        "meaning": "뛰어난, 멋진"
     },
     {
-        "word": "remain",
-        "meaning": "남아 있다, 머무르다"
+        "word": "antibody",
+        "meaning": "항체"
     },
     {
-        "word": "transplant",
-        "meaning": "옮겨 심다, 이식하다"
+        "word": "dispose",
+        "meaning": "배치하다, 배열하다, 처분하다, 없애다, 처리하다, 해결하다, 경향을 갖게 하다"
     },
     {
-        "word": "underlie",
-        "meaning": "의 기저를 이루다"
+        "word": "anticipate",
+        "meaning": "예상하다, 예측하다, 기대하다"
     },
     {
-        "word": "interfere",
-        "meaning": "간섭하다, 방해하다"
+        "word": "advance",
+        "meaning": "진보하다, 전진, 진보, 전진하다, 진보시키다"
     },
     {
-        "word": "record",
-        "meaning": "기록, 음반, 기록하다"
+        "word": "inexpensive",
+        "meaning": "비싸지 않은, 저렴한"
     },
     {
-        "word": "explain",
-        "meaning": "설명하다"
+        "word": "antique",
+        "meaning": "고대의, 골동품"
     },
     {
-        "word": "recover",
-        "meaning": "회복하다, 되찾다"
+        "word": "suggest",
+        "meaning": "제안하다. 제의하다, 암시하다, 시사하다"
     },
     {
-        "word": "transform",
-        "meaning": "변형시키다"
+        "word": "superior",
+        "meaning": "우수한, 보다 나은, 상위의, 상급의, 상급자"
     },
     {
-        "word": "demonstrate",
-        "meaning": "입증하다, 보여주다"
+        "word": "upset",
+        "meaning": "기분이 상한, 상하게 하다, 화난, 탈이 난, 뒤집어 엎다"
     },
     {
-        "word": "replace",
-        "meaning": "대신하다, 교체하다"
+        "word": "unlock",
+        "meaning": "열다"
     },
     {
-        "word": "detect",
-        "meaning": "발견하다"
+        "word": "unlikely",
+        "meaning": "~할 것 같지 않은, 가망 없는"
     },
     {
-        "word": "undergo",
-        "meaning": "겪다, 경험하다"
+        "word": "antarctic",
+        "meaning": "남극의, 남극 대륙"
     },
     {
-        "word": "undertake",
-        "meaning": "맡다, -의 책임을 지다"
+        "word": "suffer",
+        "meaning": "겪다, ~로 괴로워하다, 앓다"
     },
     {
-        "word": "diameter",
-        "meaning": "지름, 직경"
+        "word": "independent",
+        "meaning": "독립한, 자주의, 독립심이 강한"
     },
     {
-        "word": "interaction",
-        "meaning": "상호 작용, 상호 영향"
+        "word": "illegal",
+        "meaning": "불법의"
     },
     {
-        "word": "translate",
-        "meaning": "번역하다"
+        "word": "ancestor",
+        "meaning": "선조, 조상"
     },
     {
-        "word": "transfer",
-        "meaning": "이동시키다, 전학하다"
+        "word": "immune",
+        "meaning": "면역성을 가진, 면제된"
     },
     {
-        "word": "expose",
-        "meaning": "노출시키다, 드러내다"
+        "word": "uphold",
+        "meaning": "지지하다, 옹호하다"
     },
     {
-        "word": "interpret",
-        "meaning": "통역하다, 해석하다"
-    },
-    {
-        "word": "exhaust",
-        "meaning": "지치게 하다, 고갈시키다, 배기가스"
-    },
-    {
-        "word": "debate",
-        "meaning": "토론, 논쟁, 토론하다"
+        "word": "unfair",
+        "meaning": "불공평한, 부당한"
     }
 ];
 
@@ -272,7 +264,7 @@ const WordSection = ({index, value}) => {
 					placeholder={"단어를 입력하세요."}
 					onChange={(e) => setWord(e.target.value)}
 					// onChange={(e) => {
-					// 	temp[index] = {...temp[index], word: e.target.value};
+					// ue};	temp[index] = {...temp[index], word: e.target.val
 					// 	console.log(temp)
 					// }}
 					disabled={isDisabled}
