10d8e10908668e68d92d1a4457a0d7ecb8f4cf61
Jwon <check4545@kakao.com>
2023-08-20 10:41:53 +0900

    Extract Word

---
+++ src/Word.js
@@ -0,0 +1,181 @@
+const Word = [
+	{
+		"word": "conform",
+		"meaning": "순응하다, 따르다"
+	},
+	{
+		"word": "confront",
+		"meaning": "직면하다, -에 맞서다"
+	},
+	{
+		"word": "complain",
+		"meaning": "불평하다, 항의하다"
+	},
+	{
+		"word": "compromise",
+		"meaning": "타협, 절충, 타협하다, 절충하다"
+	},
+	{
+		"word": "encounter",
+		"meaning": "직면하다, 우연히 만나다, 우연히 만남"
+	},
+	{
+		"word": "select",
+		"meaning": "선택하다, 고르다"
+	},
+	{
+		"word": "synthetic",
+		"meaning": "합성한, 인조의, 종합적인"
+	},
+	{
+		"word": "multiple",
+		"meaning": "다수의, 배수의, 다양한, 배수"
+	},
+	{
+		"word": "perfect",
+		"meaning": "완전한, 무결한, 완벽한, 완벽하게 하다"
+	},
+	{
+		"word": "automobile",
+		"meaning": "자동차, 자동차의"
+	},
+	{
+		"word": "autobiography",
+		"meaning": "자서전"
+	},
+	{
+		"word": "correspond",
+		"meaning": "해당하다, 대응하다, 일치하다, 부합하다, 서신을 주고받다"
+	},
+	{
+		"word": "symphony",
+		"meaning": "교향곡, 심포니"
+	},
+	{
+		"word": "compose",
+		"meaning": "구성하다, 작곡하다, 쓰다"
+	},
+	{
+		"word": "secure",
+		"meaning": "안전한, 확실한, 확보하다, 안전하게 하다, 위험으로부터 보호하다"
+	},
+	{
+		"word": "enable",
+		"meaning": "할 수 있게 하다, 가능하게 하다"
+	},
+	{
+		"word": "multitude",
+		"meaning": "다수, 군중"
+	},
+	{
+		"word": "enclose",
+		"meaning": "둘러싸다, 동봉하다"
+	},
+	{
+		"word": "engage",
+		"meaning": "참여하다, 관여하다, 종사시키다, 끌다, 사로잡다, 약속하다, 약혼하다"
+	},
+	{
+		"word": "persevere",
+		"meaning": "노력하다, 인내하다, 견디가"
+	},
+	{
+		"word": "compile",
+		"meaning": "편집하다, 편찬하다, 수집하다"
+	},
+	{
+		"word": "company",
+		"meaning": "회사, 친구, 동료, 일행, 동석, 동행"
+	},
+	{
+		"word": "abnormal",
+		"meaning": "비정상적인"
+	},
+	{
+		"word": "enhance",
+		"meaning": "향상하다"
+	},
+	{
+		"word": "persuade",
+		"meaning": "설득하다, 납득시키다, 권해서 하게 하다, 믿게하다"
+	},
+	{
+		"word": "embrace",
+		"meaning": "껴안다, 수락하다, 받아들이다, 포옹, 수락"
+	},
+	{
+		"word": "permanent",
+		"meaning": "영구적인, 불변의, 오랫동안 지속되는, 정규의, 정규직의"
+	},
+	{
+		"word": "telescope",
+		"meaning": "망원경"
+	},
+	{
+		"word": "multimedia",
+		"meaning": "멀티미디어, 멀티미디어의"
+	},
+	{
+		"word": "perspective",
+		"meaning": "관점, 시각, 견해, 원근법, 균형 잡힌 관점, 사리분별, 사리분별력"
+	},
+	{
+		"word": "collapse",
+		"meaning": "붕괴되다, 쓰러지다, 붕괴, 쓰러짐, 실패, 무너지다"
+	},
+	{
+		"word": "condense",
+		"meaning": "농축시키다, 응축시키다, 농축되다, 응축되다, 간략하게 하다, 요약하다"
+	},
+	{
+		"word": "autograph",
+		"meaning": "사인, 자필 서명, 자필 서명하다"
+	},
+	{
+		"word": "absolute",
+		"meaning": "완전한, 전적인, 절대적인, 확실한, 전제적인, 무제한의"
+	},
+	{
+		"word": "combine",
+		"meaning": "결합하다, 결합되다, 겸비하다"
+	},
+	{
+		"word": "enforce",
+		"meaning": "시행하다, 집행하다, 강요하다"
+	},
+	{
+		"word": "separate",
+		"meaning": "분리된, 갈라진, 별개의, 따로따로의, 분리하다, 갈라놓다, 분리되다, 갈라지다"
+	},
+	{
+		"word": "enlarge",
+		"meaning": "크게 하다, 확장하다, 확장되다"
+	},
+	{
+		"word": "absorb",
+		"meaning": "흡수하다, 열중하게 하다, 받아들이다"
+	},
+	{
+		"word": "enrich",
+		"meaning": "풍부하게 하다, 부유하게 하다, 높이다"
+	},
+	{
+		"word": "absurd",
+		"meaning": "어리석은, 터무니없는, 불합리한"
+	},
+	{
+		"word": "persist",
+		"meaning": "계속 - 하다, 지속하다, 고집하다, 계속하다, 존속하다"
+	},
+	{
+		"word": "entitle",
+		"meaning": "자격을 주다, 표제를 붙이다, 권리를 주다, 명칭을 붙이다"
+	},
+	{
+		"word": "confirm",
+		"meaning": "확인하다, 확정하다, 보여주다, 확인해주다, 인정하다"
+	}
+];
+
+
+export default Word;
